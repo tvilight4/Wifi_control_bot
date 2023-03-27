1cf325eb7e314e50f8979d66501a0c1af84f9048
tvilight4 <122543856+tvilight4@users.noreply.github.com>
2023-03-27 22:32:03 +0530

    Add files via upload

--- 7_line_following/main/line_following.c
+++ 7_line_following/main/line_following.c
@@ -35,75 +35,8 @@ float error=0, prev_error=0, difference, cumulative_error, correction;
 line_sensor_array line_sensor_readings;
 
 
-void lsa_to_bar()
-{   
-    uint8_t var = 0x00;                     
-    bool number[8] = {0,0,0,0,0,0,0,0};
-    for(int i = 0; i < 5; i++)
-    {
-        number[7-i] = (line_sensor_readings.adc_reading[i] < BLACK_BOUNDARY) ? 0 : 1; //If adc value is less than black margin, then set that bit to 0 otherwise 1.
-        var = bool_to_uint8(number);  //A helper function to convert bool array to unsigned int.
-        ESP_ERROR_CHECK(set_bar_graph(var)); //Setting bar graph led with unsigned int value.
-    }
-}
 
-void calculate_correction()
-{
-    error = error*10;  // we need the error correction in range 0-100 so that we can send it directly as duty cycle paramete
-    difference = error - prev_error;
-    cumulative_error += error;
 
-    cumulative_error = bound(cumulative_error, -30, 30);
-
-    correction = read_pid_const().kp*error + read_pid_const().ki*cumulative_error + read_pid_const().kd*difference;
-    prev_error = error;
-}
-
-void calculate_error()
-{
-    int all_black_flag = 1; // assuming initially all black condition
-    float weighted_sum = 0, sum = 0; 
-    float pos = 0; int k = 0;
-
-    for(int i = 0; i < 5; i++)
-    {
-        if(line_sensor_readings.adc_reading[i] > BLACK_BOUNDARY)
-        {
-            all_black_flag = 0;
-        }
-        if(line_sensor_readings.adc_reading[i] > 700)
-        {
-            k = 1;
-        }
-        if(line_sensor_readings.adc_reading[i] < 700)
-        {
-            k = 0;
-        }
-        weighted_sum += (float)(weights[i]) * k;
-        sum = sum + k;
-    }
-
-    if(sum != 0) // sum can never be 0 but just for safety purposes
-    {
-        pos = (weighted_sum - 1) / sum; // This will give us the position wrt line. if +ve then bot is facing left and if -ve the bot is facing to right.
-    }
-
-    if(all_black_flag == 1)  // If all black then we check for previous error to assign current error.
-    {
-        if(prev_error > 0)
-        {
-            error = 2.5;
-        }
-        else
-        {
-            error = -2.5;
-        }
-    }
-    else
-    {
-        error = pos;
-    }
-}
 
 void line_follow_task(void* arg)
 {
@@ -122,35 +55,10 @@ void line_follow_task(void* arg)
 
     while(true)
     {
-        line_sensor_readings = read_line_sensor();
-        for(int i = 0; i < 5; i++)
-        {
-            line_sensor_readings.adc_reading[i] = bound(line_sensor_readings.adc_reading[i], WHITE_MARGIN, BLACK_MARGIN);
-            line_sensor_readings.adc_reading[i] = map(line_sensor_readings.adc_reading[i], WHITE_MARGIN, BLACK_MARGIN, bound_LSA_LOW, bound_LSA_HIGH);
-            line_sensor_readings.adc_reading[i] = 1000 - (line_sensor_readings.adc_reading[i]);
-        }
-
-        calculate_error();
-        calculate_correction();
-        lsa_to_bar();
-
-        left_duty_cycle = bound((optimum_duty_cycle + correction), lower_duty_cycle, higher_duty_cycle);
-        right_duty_cycle = bound((optimum_duty_cycle - correction), lower_duty_cycle, higher_duty_cycle);
-
-        set_motor_speed(MOTOR_A_0, MOTOR_FORWARD, left_duty_cycle);
-        set_motor_speed(MOTOR_A_1, MOTOR_FORWARD, right_duty_cycle);
-
+       
 
         //ESP_LOGI("debug","left_duty_cycle:  %f    ::  right_duty_cycle :  %f  :: error :  %f  correction  :  %f  \n",left_duty_cycle, right_duty_cycle, error, correction);
-       ESP_LOGI("debug", "X: %f ::  Y: %f  :: SPEED: %f :: ANGLE: %f", read_analog_const().x, read_analog_const().y, read_analog_const().speed, read_analog_const().angle);
-#ifdef CONFIG_ENABLE_OLED
-        // Diplaying kp, ki, kd values on OLED 
-        if (read_pid_const().val_changed)
-        {
-            display_pid_values(read_pid_const().kp, read_pid_const().ki, read_pid_const().kd);
-            reset_val_changed_pid_const();
-        }
-#endif
+        ESP_LOGI("debug", "X: %f ::  Y: %f  :: SPEED: %f :: ANGLE: %f", read_pid_const().x, read_pid_const().y, read_pid_const().speed, read_pid_const().angle);
 
         vTaskDelay(10 / portTICK_PERIOD_MS);
     }
